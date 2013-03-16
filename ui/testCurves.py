db4595c85ba00b549432744fb5b717ca92ac96ef
Nop Jiarathanakul <prutsdomj@gmail.com>
2013-03-16 19:50:09 -0400

    working intersections script in python

--- ui/testCurves.py
+++ ui/testCurves.py
@@ -2,17 +2,43 @@ from sympy import *
 import maya.cmds as cmds
 import maya.mel as mel
 
-curves = mel.eval("ls -sl")
-intersects = [float(i) for i in cmds.curveIntersect(curves[0], curves[1], useDirection=True, direction=(0,0,1)).split()]
-pi = cmds.pointOnCurve(curves[0], pr=intersects[0], p=True)
-pj = cmds.pointOnCurve(curves[1], pr=intersects[1], p=True)
-ni = cmds.pointOnCurve(curves[0], pr=intersects[0], nn=True)
-nj = cmds.pointOnCurve(curves[1], pr=intersects[1], nn=True)
-ti = cmds.pointOnCurve(curves[0], pr=intersects[0], t=True)
-tj = cmds.pointOnCurve(curves[1], pr=intersects[1], t=True)
+crossHairPos
+crossSections
 
-cmds.spaceLocator(p=pi)
 
+mel.eval("layerEditorSelectObjects layerCross;")
+crossSections = mel.eval('ls -sl -type "transform"')
+
+# no curve drawn
+if not crossSections:
+  return
+
+numCrossSections = len(crossSections)
+crossHairPos = [[0]*numCrossSections for x in range(numCrossSections, 0, -1)]
+
+for ci in range(numCrossSections):
+  for cj in range(ci+1, numCrossSections):
+
+    rawIntersects = cmds.curveIntersect(crossSections[ci], crossSections[cj], useDirection=True, direction=(0,0,1))
+    
+    if rawIntersects:
+      intersects = [float(i) for i in rawIntersects.split()]
+      
+      pi = cmds.pointOnCurve(crossSections[ci], pr=intersects[0], p=True)
+
+      # place locator
+      cmds.spaceLocator(p=pi)
+      
+      # save
+      crossHairPos[ci][cj] = pi;
+      
+      print "(%s,%s)" % (ci, cj)
+      
+    else:
+      print "no curve intersect"
+
+
+'''
 niz = Symbol("niz")
 njz = Symbol("njz")
 tiz = Symbol("tiz")
@@ -24,4 +50,5 @@ solution = solve(
     ti[0]*ni[0] + ti[1]*ni[1] + tiz*niz,
     tj[0]*nj[0] + tj[1]*nj[1] + tjz*njz
   ], 
-  niz, njz, tiz, tjz)
+  niz, njz, tiz, tjz)  
+'''
