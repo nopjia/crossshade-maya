d592d2be7daa9d81caa974f7e03c4c42a0e5bf3c
Nop Jiarathanakul <prutsdomj@gmail.com>
2013-04-16 23:55:02 -0400

    successfully ran optimization

--- testCurves.py
+++ testCurves.py
@@ -89,7 +89,7 @@ def minOptimize():
   # construct constraints
   global consList
   consList = []
-  tanPairIdx = 0
+  tanPairIdx = csN*2
   for i in range(csN):
     for j in range(i+1, csN):
       if (chT[i][j]):
@@ -97,63 +97,58 @@ def minOptimize():
         # -e < n_i . n_j < e
         consList.append({
           "type": "ineq",
-          "fun" : lambda x: (x[i*2]*x[j*2] + x[i*2+1]*x[j*2+1] + 1.0) + EPSILON,
-          "test" : "(x[%s]*x[%s] + x[%s]*x[%s] + 1.0) + e" % (i*2,j*2,i*2+1,j*2+1)
+          "fun" : (lambda i,j: lambda x: (x[i*2]*x[j*2] + x[i*2+1]*x[j*2+1] + 1.0) + EPSILON)(i,j)
         })
         consList.append({
           "type": "ineq",
-          "fun" : lambda x: -(x[i*2]*x[j*2] + x[i*2+1]*x[j*2+1] + 1.0) + EPSILON,
-          "test" : "-(x[%s]*x[%s] + x[%s]*x[%s] + 1.0) + e" % (i*2,j*2,i*2+1,j*2+1)
+          "fun" : (lambda i,j: lambda x: -(x[i*2]*x[j*2] + x[i*2+1]*x[j*2+1] + 1.0) + EPSILON)(i,j)
         })
         
         # -e < t_ij . t_ji < e
         consList.append({
           "type": "ineq",
-          "fun" : lambda x: 
-            (chT[i][j][0]*chT[j][i][0] + chT[i][j][1]*chT[j][i][1] + x[csN*2+tanPairIdx]*x[csN*2+1+tanPairIdx]) + EPSILON,
-          "test": "(chT[i][j][0]*chT[j][i][0] + chT[i][j][0]*chT[j][i][0] + x[%s]*x[%s]) + e" % (csN*2+tanPairIdx, csN*2+1+tanPairIdx)            
+          "fun" : (lambda tijx, tijy, tjix, tjiy, tanPairIdx:
+            lambda x: (tijx*tjix + tijy*tjiy + x[tanPairIdx]*x[tanPairIdx+1]) + EPSILON)
+            (chT[i][j][0], chT[i][j][1], chT[j][i][0], chT[j][i][1], tanPairIdx)
         })
         consList.append({
           "type": "ineq",
-          "fun" : lambda x: 
-            -(chT[i][j][0]*chT[j][i][0] + chT[i][j][1]*chT[j][i][1] + x[csN*2+tanPairIdx]*x[csN*2+1+tanPairIdx]) + EPSILON,
-          "test": "-(chT[i][j][0]*chT[j][i][0] + chT[i][j][0]*chT[j][i][0] + x[%s]*x[%s]) + e" % (csN*2+tanPairIdx, csN*2+1+tanPairIdx)
+          "fun" : (lambda tijx, tijy, tjix, tjiy, tanPairIdx:
+            lambda x: -(tijx*tjix + tijy*tjiy + x[tanPairIdx]*x[tanPairIdx+1]) + EPSILON)
+            (chT[i][j][0], chT[i][j][1], chT[j][i][0], chT[j][i][1], tanPairIdx)
         })
         
         # t_ij . n_i = 0
         consList.append({
           "type": "eq",
-          "fun" : lambda x: (chT[i][j][0]*x[i*2] + chT[i][j][1]*x[i*2+1] + x[csN*2+tanPairIdx]),
-          "test": "(chT[i][j][0]*x[i*2] + chT[i][j][1]*x[i*2+1] + x[%s])" % (csN*2+tanPairIdx)
+          "fun" : (lambda tijx, tijy, i, tanPairIdx:
+            lambda x: (tijx*x[i*2] + tijy*x[i*2+1] + x[tanPairIdx]) )
+            (chT[i][j][0], chT[i][j][1], i, tanPairIdx)
         })
         # t_ji . n_j = 0
         consList.append({
           "type": "eq",
-          "fun" : lambda x: (chT[j][i][0]*x[j*2] + chT[j][i][1]*x[j*2+1] + x[csN*2+tanPairIdx+1]),
-          "test": "(chT[i][j][0]*x[i*2] + chT[i][j][1]*x[i*2+1] + x[%s])" % (csN*2+tanPairIdx+1)
+          "fun" : (lambda tjix, tjiy, j, tanPairIdx:
+            lambda x: (tjix*x[j*2] + tjiy*x[j*2+1] + x[tanPairIdx+1]) )
+            (chT[j][i][0], chT[j][i][1], j, tanPairIdx)
         })
         
         print "cons for (%s,%s)" % (i, j)
         
         tanPairIdx += 2
-    
-  # print constraints
-  print "CONSTRAINTS:"
-  for i in consList:
-    print i["test"]
   
   # construct cost function
   funcString = "0"
-  tanPairIdx = 0
+  tanPairIdx = csN*2
   for i in range(csN):
     for j in range(i+1, csN):
       if (chT[i][j]):
         ni = "np.array([x["+str(i*2)+"], x["+str(i*2+1)+"], 1])"
         nj = "np.array([x["+str(j*2)+"], x["+str(j*2+1)+"], 1])"
-        tij = "np.array(["+str(chT[i][j][0])+", "+str(chT[i][j][1])+", x["+str(csN*2+tanPairIdx)+"]])"
-        tji = "np.array(["+str(chT[j][i][0])+", "+str(chT[j][i][1])+", x["+str(csN*2+tanPairIdx+1)+"]])"
+        tij = "np.array(["+str(chT[i][j][0])+", "+str(chT[i][j][1])+", x["+str(tanPairIdx)+"]])"
+        tji = "np.array(["+str(chT[j][i][0])+", "+str(chT[j][i][1])+", x["+str(tanPairIdx+1)+"]])"
         
-        sum = "((np.power(np.linalg.norm(np.cross("+tji+","+ni+")),2)) + (np.power(np.linalg.norm(np.cross("+tij+","+nj+")),2)) + np.power(x["+str(csN*2+tanPairIdx)+"],2) + np.power(x["+str(csN*2+tanPairIdx+1)+"],2))"
+        sum = "((np.power(np.linalg.norm(np.cross("+tji+","+ni+")),2)) + (np.power(np.linalg.norm(np.cross("+tij+","+nj+")),2)) + np.power(x["+str(tanPairIdx)+"],2) + np.power(x["+str(tanPairIdx+1)+"],2))"
         
         funcString += "+"+sum
         
