4618d09ac560c2993d9a9aaf050a9fbdd60c44ca
Nop Jiarathanakul <prutsdomj@gmail.com>
2013-04-30 00:24:42 -0400

    seems to have working single interp function

--- testCurves.py
+++ testCurves.py
@@ -1,15 +1,25 @@
 import maya.cmds as cmds
 import maya.mel as mel
 import numpy as np
-from scipy.optimize import minimize
+import scipy as sp
 
 def normalize(v):
   return v/np.linalg.norm(v)
+
+# angle t
+# axis u
+def rotation(t, u):
+  return [
+    [np.cos(t)+u[0]*u[0], u[0]*u[1]*(1-np.cos(t))-u[2]*np.sin(t), u[0]*u[2]*(1-np.cos(t))+u[1]*np.sin(t)],
+    [u[1]*u[0]*(1-np.cos(t))+u[2]*np.sin(t), np.cos(t)+u[1]*u[1], u[1]*u[2]*(1-np.cos(t))-u[0]*np.sin(t)],
+    [u[2]*u[0]*(1-np.cos(t))-u[1]*np.sin(t), u[2]*u[1]*(1-np.cos(t))+u[0]*np.sin(t), np.cos(t)+u[2]*u[2]]
+  ]
 
 csNum = 0       # cross section count number
 csNam = None    # cross section curves names
 csNor = None    # cross section normals
 chNum = 0       # cross hair count number
+chTee = None    # cross hair t-parameter
 chPos = None    # cross hair positions
 chTan = None    # cross hair tangents
 chNor = None    # cross hair normals
@@ -19,6 +29,7 @@ def readCrossSections():
   global csNam
   global csNor
   global chNum
+  global chTee
   global chPos
   global chTan
   global chNor
@@ -35,40 +46,42 @@ def readCrossSections():
   csNum = len(csNam)
   csNor = [None for x in range(csNum)]
   chNum = 0
-  chPos = [[None]*csNum for x in range(csNum, 0, -1)]  
+  chTee = [[-1]*csNum for x in range(csNum, 0, -1)]
+  chPos = [[None]*csNum for x in range(csNum, 0, -1)]
   chTan = [[None]*csNum for x in range(csNum, 0, -1)]
   chNor = [[None]*csNum for x in range(csNum, 0, -1)]
 
   
   # process intersections
   
-  for ci in range(csNum):
-    for cj in range(ci+1, csNum):
+  for i in range(csNum):
+    for j in range(i+1, csNum):
 
-      rawIntersects = cmds.curveIntersect(csNam[ci], csNam[cj], useDirection=True, direction=(0,0,1))
+      rawIntersects = cmds.curveIntersect(csNam[i], csNam[j], useDirection=True, direction=(0,0,1))
       
       if rawIntersects:
-        intersects = [float(i) for i in rawIntersects.split()]
-        intT_i = intersects[0]
-        intT_j = intersects[1]
+        intersects = [float(k) for k in rawIntersects.split()]
+        chTee[i][j] = intersects[0]
+        chTee[j][i] = intersects[1]
         
-        pi = cmds.pointOnCurve(csNam[ci], pr=intT_i, p=True)        
-        chPos[ci][cj] = np.array(pi)  # save
+        pi = cmds.pointOnCurve(csNam[i], pr=chTee[i][j], p=True)        
+        chPos[i][j] = np.array(pi)  # save
+        chPos[j][i] = np.array(pi)
         #cmds.spaceLocator(p=pi)   # place locator        
         
-        t_ij = cmds.pointOnCurve(csNam[ci], pr=intT_i, nt=True)
-        chTan[ci][cj] = np.array(t_ij)
-        #cmds.spaceLocator( p=(chPos[ci][cj]+chTan[ci][cj]).tolist() )
-        t_ji = cmds.pointOnCurve(csNam[cj], pr=intT_j, nt=True)
-        chTan[cj][ci] = np.array(t_ji)
-        #cmds.spaceLocator( p=(chPos[ci][cj]+chTan[cj][ci]).tolist() )
+        t_ij = cmds.pointOnCurve(csNam[i], pr=chTee[i][j], nt=True)
+        chTan[i][j] = np.array(t_ij)
+        #cmds.spaceLocator( p=(chPos[i][j]+chTan[i][j]).tolist() )
+        t_ji = cmds.pointOnCurve(csNam[j], pr=chTee[j][i], nt=True)
+        chTan[j][i] = np.array(t_ji)
+        #cmds.spaceLocator( p=(chPos[i][j]+chTan[j][i]).tolist() )
         
-        print "(%s,%s) processed" % (ci, cj)
+        print "(%s,%s) processed" % (i, j)
         
         chNum += 1
         
       else:
-        print "(%s,%s) no intersect" % (ci, cj)
+        print "(%s,%s) no intersect" % (i, j)
   
   # clear all selection
   mel.eval("select -cl")
@@ -78,6 +91,7 @@ def printCrossSectionData1():
   global csNam
   global csNor
   global chNum
+  global chTee
   global chPos
   global chTan
   global chNor
@@ -87,18 +101,19 @@ def printCrossSectionData1():
   
   for i in range(csNum):
     for j in range(csNum):
-      print "x_(%s,%s) : %s" % (i, j, chPos[i][j])
+      print "x_(%s,%s) : at %s : %s" % (i, j, chTee[i][j], chPos[i][j])
       
   for i in range(csNum):
     for j in range(csNum):
       print "t_(%s,%s) : %s" % (i, j, chTan[i][j])
 
-consList
+#consList
 def minOptimize(): 
   global csNum
   global csNam
   global csNor
   global chNum
+  global chTee
   global chPos
   global chTan
   global chNor
@@ -152,7 +167,7 @@ def minOptimize():
             (chTan[j][i][0], chTan[j][i][1], j, tanPairIdx)
         })
         
-        print "cons for (%s,%s)" % (i, j)
+        # print "constraints for (%s,%s)" % (i, j)
         
         tanPairIdx += 2
   
@@ -182,7 +197,7 @@ def minOptimize():
   
   # run optimization
   x0 = [-1 for x in range(csNum*2+chNum*2)]  # initial guess
-  res = minimize(cmFunction, x0, method='SLSQP', constraints=tuple(consList), options={'disp': True})
+  res = sp.optimize.minimize(cmFunction, x0, method='SLSQP', constraints=tuple(consList), options={'disp': True})
   
   # store cross section plane normals
   for i in range(csNum):
@@ -219,6 +234,36 @@ def minOptimize():
         
         cmds.spaceLocator( p=(chPos[i][j]+chNor[i][j]).tolist() )
 
+# get interpolated normal of ch_ij along curve i at t
+def getCHNormAtT(chI, chJ, tparam):
+  global csNum
+  global csNam
+  global csNor
+  global chNum
+  global chTee
+  global chPos
+  global chTan
+  global chNor
+
+  origN = chNor[chI][chJ]
+  origT1 = chTan[chI][chJ]
+  origT2 = chTan[chJ][chI]
+  targetT = cmds.pointOnCurve(csNam[chI], pr=tparam, nt=True)   # z comp incorrect
+  targetT[2] = -(origT2[0]*targetT[0]+origT2[1]*targetT[1])/origT2[2]   # solve for z comp
+    
+  crossProd = np.cross(origT1, targetT)
+  axis = normalize(crossProd)
+  angle = np.arcsin( np.linalg.norm(crossProd) / (np.linalg.norm(origT1)*np.linalg.norm(targetT)) )
+  rot = rotation(angle,axis)
+  
+  return normalize(np.dot(rot, origN))
+        
+def propagateCurve():
+  print "propagate along curve"
+
+def propagatePatch():
+  print "propagate coons patch"
+        
 #----------------------------------------------------------
 # RUN COMMAND
 #----------------------------------------------------------
@@ -227,5 +272,7 @@ def runCrossShade():
   readCrossSections()
   printCrossSectionData1()
   minOptimize()
+  propagateCurve()
+  propagatePatch()
   
-#runCrossShade()
+runCrossShade()
